0c07f360368b801297518a4292ce2a35a0592d2e
MohitRam <mohitram.396@gmail.com>
2024-05-15 19:33:19 +0100

    First Project Page creation

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="container-fluid ">
+  <footer class="py-2  border-top">
+    <p class="text-center text-muted">Mohit Ram</p>
+  </footer>
+</div>
+  )
+}
+
+export default Footer
---
+++ src/projects/project001/ProjectPage001.jsx
@@ -0,0 +1,16 @@
+
+import Content001 from "./Content001";
+import StickyTopBar from "../../components/StickyTopBar/StickyTopBar";
+import Footer from "../../components/Footer/Footer";
+const ProjectPage001 = () => {
+  return (
+    <div>
+      <StickyTopBar />
+      {/* <Content001 /> */}
+      <Content001 />
+      <Footer />
+    </div>
+  );
+};
+
+export default ProjectPage001;
